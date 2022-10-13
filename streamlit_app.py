6878ed3641d2133ef2d2edefbcca28db15ecc5cf
Snowflake-Saaho <115687062+Snowflake-Saaho@users.noreply.github.com>
2022-10-13 11:41:23 +0530

    Update streamlit_app.py
    Added Header and some text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Dosa 2 & Idly 2')
+streamlit.text('Mango, Grapes, Blueberry & Blackberry Smoothies')
+streamlit.text('Milk, Curd, Butter')
+streamlit.text('Boiled-Eggs, Bread Omlette')
