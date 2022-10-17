aeabd2437786b24a3173257cffc81e0f0629e4ad
Snowflake-Saaho <115687062+Snowflake-Saaho@users.noreply.github.com>
2022-10-17 11:19:09 +0530

    Update streamlit_app.py
    error resolved

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,4 @@ streamlit.dataframe(my_data_rows)
 streamlit.header("Add Fruit!")
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Add fruit...')
-streamlit.write('The user entered ', add_my_fruit)
-streamlit.text('Thanks for adding, add_my_fruit)
+streamlit.write('Thanks for adding', add_my_fruit)
